9f0fafccc22f16fe0968680d50ebdfb129008375
Marjan Ilovski <ilovski5@gmail.com>
2026-03-10 15:24:51 +0100

    test: astro and workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 const site = import.meta.env.PROD
   ? "https://marjanilovski.com"
   : "http://localhost:4321";
 
-// https://astro.build/config
 export default defineConfig({
   site,
+  trailingSlash: "never",
   integrations: [sitemap(), icon()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
+  build: { format: "file" },
+  devToolbar: { enabled: false },
 });
